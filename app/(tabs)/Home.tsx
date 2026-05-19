23a7f734a5eefc7dbf3998057ac07d1374235eac
Hyonam99 <victorbassey69@yahoo.com>
2026-05-19 21:59:01 +0100

    feat(tabs): implement app menu tabs

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View className='flex-1 items-center justify-center'>
+    <SafeAreaView className='flex-1 bg-background p-5'>
       <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,73 @@
+import { tabs } from "@/constants/data";
+import { colors, components } from "@/constants/theme";
+import clsxStyle from "clsx";
 import { Tabs } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const TabLayout = () => {
+	const insets = useSafeAreaInsets();
+	const tabBarTheme = components.tabBar;
+
 	return (
 		<>
-			<StatusBar style="auto" />
+			<StatusBar style="auto" hidden/>
 			<Tabs
 				screenOptions={{
 					headerShown: false,
+					tabBarShowLabel: false,
+					tabBarStyle: {
+						position: "absolute",
+						bottom: Math.max(
+							insets.bottom,
+							tabBarTheme.horizontalInset,
+						),
+						height: tabBarTheme.height,
+						marginHorizontal: tabBarTheme.horizontalInset,
+						borderRadius: tabBarTheme.radius,
+						backgroundColor: colors.primary,
+						borderTopWidth: 0,
+						elevation: 0,
+					},
+					tabBarItemStyle: {
+						paddingVertical:
+							tabBarTheme.height / 2 -
+							tabBarTheme.iconFrame / 1.6,
+					},
+					tabBarIconStyle: {
+						alignItems: "center",
+						height: tabBarTheme.iconFrame,
+						width: tabBarTheme.iconFrame,
+					},
 				}}
 			>
-				<Tabs.Screen
-					name="Home"
-					options={{
-						title: "Home",
-					}}
-				/>
+				{tabs.map((tab) => (
+					<Tabs.Screen
+						key={tab.name}
+						name={tab.name}
+						options={{
+							title: tab.title,
+							tabBarIcon: ({ focused }) => (
+								<View className="tabs-icon">
+									<View
+										className={clsxStyle(
+											"tabs-pill",
+											focused && "tabs-active",
+										)}
+									>
+										<Image
+											source={tab.icon}
+											resizeMode="contain"
+											className="tabs-glyph"
+										/>
+									</View>
+								</View>
+							),
+						}}
+					/>
+				))}
 			</Tabs>
 		</>
 	);
