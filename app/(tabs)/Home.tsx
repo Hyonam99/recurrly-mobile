a6f454dba8ade6b76a42e800cce0645d4ee16dc8
Hyonam99 <victorbassey69@yahoo.com>
2026-05-22 18:41:34 +0100

    stable version 0.01 from metro bundler issue

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,13 +1,20 @@
-import { Text } from 'react-native'
-import React from 'react'
+import { HOME_USER } from "@/constants/data";
+import images from "@/constants/images";
+import React from "react";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
-  return (
-    <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text>Home</Text>
-    </SafeAreaView>
-  )
-}
+	return (
+		<SafeAreaView className="flex-1 bg-background p-5">
+			<View className="home-header">
+				<View className="home-user">
+					<Image source={images.avatar} className="home-avatar" />
+					<Text className="home-user-name">{HOME_USER.name}</Text>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
+};
 
-export default Home
+export default Home;
